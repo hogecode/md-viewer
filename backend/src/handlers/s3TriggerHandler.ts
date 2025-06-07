943ef9b76b3b1119b87c00af44ec8730178842f5
shun103100000 <shun103100000@gmail.com>
2025-06-07 17:02:36 +0900

    refactor: template.ymlのTS対応とCodeUriの追加

--- backend/src/handlers/s3TriggerHandler.ts
+++ backend/src/handlers/s3TriggerHandler.ts
@@ -2,7 +2,7 @@
 /**
  * S3トリガーでMDをRDBに保存する関数
  */
-export const handler = async (event) => {
+export const handler = async (event: any) => {
   // S3キーからユーザーidなどのメタデータを取得
   // MDファイルをXSS対策して文字列に変換
   // ユーザーidと共にRDBに保存
